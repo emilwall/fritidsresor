d0a03b6e97727c75c3a9def24c7c5bd3b41d067a
Emil Wall <emil.wall@valtech.se>
2013-04-16 12:00:09 +0100

    Fix inheritance in APIClient

---
+++ fritidsresor/VTLastMinuteApiClient.h
@@ -0,0 +1,14 @@
+//
+//  VTLastMinuteApiClient.h
+//  fritidsresor
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 William Lundin Forssén. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+@interface VTLastMinuteApiClient : AFHTTPClient
+
+@end
---
+++ fritidsresor/VTLastMinuteApiClient.m
@@ -0,0 +1,13 @@
+//
+//  VTLastMinuteApiClient.m
+//  fritidsresor
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 William Lundin Forssén. All rights reserved.
+//
+
+#import "VTLastMinuteApiClient.h"
+
+@implementation VTLastMinuteApiClient
+
+@end
