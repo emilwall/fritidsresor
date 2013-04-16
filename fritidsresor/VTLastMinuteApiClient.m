cbf94d8cda98d6023bf6899d1e12bd4a193e7f15
Emil Wall <emil.wall@valtech.se>
2013-04-16 12:01:27 +0100

    Implement VTLastMinuteApiClient

--- fritidsresor/VTLastMinuteApiClient.m
+++ fritidsresor/VTLastMinuteApiClient.m
@@ -10,4 +10,28 @@
 
 @implementation VTLastMinuteApiClient
 
++ (VTLastMinuteApiClient *)sharedClient {
+    static VTLastMinuteApiClient *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedClient = [[VTLastMinuteApiClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://travel-offers-api.apphb.com/"]];
+    });
+
+    return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+
+    // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+	[self setDefaultHeader:@"Accept" value:@"application/json"];
+
+    return self;
+}
+
 @end
